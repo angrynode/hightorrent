153be2bde8b851e1a9e63d619b427908d8aa4379
angrynode <angrynode@kl.netlib.re>
2024-09-02 18:14:40 +0200

    Ensure SingleTarget matches truncated V2/hybrid hash

--- src/list.rs
+++ src/list.rs
@@ -1,4 +1,4 @@
-use crate::{InfoHash, SingleTarget, Torrent};
+use crate::{SingleTarget, Torrent};
 
 /// A list of [`Torrent`](crate::torrent::Torrent), with querying/filtering capabilities.
 ///
@@ -28,16 +28,7 @@ impl TorrentList {
     pub fn get(&self, target: &SingleTarget) -> Option<Torrent> {
         self.0
             .iter()
-            .find(|t| {
-                match &t.hash {
-                    InfoHash::V1(h) | InfoHash::V2(h) => h.as_str() == target.as_str(),
-                    InfoHash::Hybrid((v1, _v2)) => {
-                        // Priority is given to matching v2, for more resilience to collision attacks
-                        // but we can still match hybrid by infohash v1 SingleTarget
-                        t.id.as_str() == target.truncated() || v1 == target.as_str()
-                    }
-                }
-            })
+            .find(|t| target.matches_hash(&t.hash))
             .map(|t| t.clone())
     }
 }
@@ -60,5 +51,145 @@ impl FromIterator<Torrent> for TorrentList {
         }
 
         c
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{InfoHash, SingleTarget, Torrent};
+
+    use super::TorrentList;
+
+    fn dummy_list() -> TorrentList {
+        TorrentList::from_vec(vec![
+            Torrent::dummy_from_hash(
+                &InfoHash::new("C811B41641A09D192B8ED81B14064FFF55D85CE3").unwrap(),
+            ),
+            Torrent::dummy_from_hash(
+                &InfoHash::new("631a31dd0a46257d5078c0dee4e66e26f73e42ac")
+                    .unwrap()
+                    .hybrid(
+                        &InfoHash::new(
+                            "d8dd32ac93357c368556af3ac1d95c9d76bd0dff6fa9833ecdac3d53134efabb",
+                        )
+                        .unwrap(),
+                    )
+                    .unwrap(),
+            ),
+            Torrent::dummy_from_hash(
+                &InfoHash::new("caf1e1c30e81cb361b9ee167c4aa64228a7fa4fa9f6105232b28ad099f3a302e")
+                    .unwrap(),
+            ),
+        ])
+    }
+
+    #[test]
+    fn matches_v1() {
+        let list = dummy_list();
+        let target = SingleTarget::new("C811B41641A09D192B8ED81B14064FFF55D85CE3").unwrap();
+
+        let found = list.get(&target).unwrap();
+
+        assert_eq!(
+            found.hash,
+            InfoHash::new("C811B41641A09D192B8ED81B14064FFF55D85CE3").unwrap()
+        );
+    }
+
+    #[test]
+    fn matches_hybrid_v2() {
+        let list = dummy_list();
+        let target =
+            SingleTarget::new("d8dd32ac93357c368556af3ac1d95c9d76bd0dff6fa9833ecdac3d53134efabb")
+                .unwrap();
+
+        let found = list.get(&target).unwrap();
+
+        assert_eq!(
+            found.hash,
+            InfoHash::new("631a31dd0a46257d5078c0dee4e66e26f73e42ac")
+                .unwrap()
+                .hybrid(
+                    &InfoHash::new(
+                        "d8dd32ac93357c368556af3ac1d95c9d76bd0dff6fa9833ecdac3d53134efabb"
+                    )
+                    .unwrap()
+                )
+                .unwrap()
+        );
+    }
+
+    #[test]
+    fn matches_hybrid_v2_truncated() {
+        let list = dummy_list();
+        let target = SingleTarget::new("d8dd32ac93357c368556af3ac1d95c9d76bd0dff").unwrap();
+
+        let found = list.get(&target).unwrap();
+
+        assert_eq!(
+            found.hash,
+            InfoHash::new("631a31dd0a46257d5078c0dee4e66e26f73e42ac")
+                .unwrap()
+                .hybrid(
+                    &InfoHash::new(
+                        "d8dd32ac93357c368556af3ac1d95c9d76bd0dff6fa9833ecdac3d53134efabb"
+                    )
+                    .unwrap()
+                )
+                .unwrap()
+        );
+    }
+
+    #[test]
+    fn matches_hybrid_v1() {
+        let list = dummy_list();
+        let target = SingleTarget::new("631a31dd0a46257d5078c0dee4e66e26f73e42ac").unwrap();
+
+        let found = list.get(&target).unwrap();
+
+        assert_eq!(
+            found.hash,
+            InfoHash::new("631a31dd0a46257d5078c0dee4e66e26f73e42ac")
+                .unwrap()
+                .hybrid(
+                    &InfoHash::new(
+                        "d8dd32ac93357c368556af3ac1d95c9d76bd0dff6fa9833ecdac3d53134efabb"
+                    )
+                    .unwrap()
+                )
+                .unwrap()
+        );
+    }
+
+    #[test]
+    fn matches_v2() {
+        let list = dummy_list();
+        let target =
+            SingleTarget::new("caf1e1c30e81cb361b9ee167c4aa64228a7fa4fa9f6105232b28ad099f3a302e")
+                .unwrap();
+
+        let found = list.get(&target).unwrap();
+
+        assert_eq!(
+            found.hash,
+            InfoHash::new("caf1e1c30e81cb361b9ee167c4aa64228a7fa4fa9f6105232b28ad099f3a302e")
+                .unwrap()
+        );
+    }
+
+    #[test]
+    fn matches_v2_truncated() {
+        let list = dummy_list();
+        let target =
+            SingleTarget::new("caf1e1c30e81cb361b9ee167c4aa64228a7fa4fa")
+                .unwrap();
+
+        let found = list.get(&target).unwrap();
+
+        assert_eq!(
+            found.hash,
+            InfoHash::new("caf1e1c30e81cb361b9ee167c4aa64228a7fa4fa9f6105232b28ad099f3a302e")
+                .unwrap()
+        );
     }
 }
