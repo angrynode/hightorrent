a8c14357b43dc71335e7a57a92b83ae5ae9afe8b
angrynode <angrynode@kl.netlib.re>
2024-09-03 21:43:50 +0200

    Add flake.nix for `nix develop` shell

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A Nix-flake-based Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ rust-overlay.overlays.default ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell rec {
+          buildInputs = with pkgs; [
+            (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default))
+            cargo-nextest
+            cargo-rdme
+            just
+          ];
+        };
+      }
+    );
+}
