4c5ea80065abfd5116ff43256726a1de33a14fec
angrynode <angrynode@kl.netlib.re>
2025-03-25 19:31:15 +0100

    ci: Add cargo-rdme when installed, explicit nightly usage

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -4,11 +4,16 @@ export RUSTFLAGS="-Dwarnings"
 export RUSTDOCFLAGS="--cfg docsrs -Dwarnings"
 
 set -e
+set -x
 
-cargo --version
-rustc --version
-cargo fmt --check
-cargo clippy --all-features
-cargo test
-cargo test --all-features
-cargo doc --no-deps -Zrustdoc-map --all-features
+cargo +nightly --version
+cargo +nightly fmt --check
+cargo +nightly clippy --all-features
+cargo +nightly test
+cargo +nightly test --all-features
+cargo +nightly doc --no-deps -Zrustdoc-map --all-features
+if command -v cargo-rdme 2>&1 >/dev/null; then
+  cargo-rdme --check
+else
+  echo "Skip cargo-rdme (not installed)"
+fi
