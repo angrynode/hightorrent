8bfdfc8653cd2fc87b0bf6dfd3a283f679429b30
angrynode <angrynode@kl.netlib.re>
2025-11-15 11:35:56 +0100

    feat: Allow usage of MagnetLink in sea_orm::Model

--- src/magnet.rs
+++ src/magnet.rs
@@ -322,6 +322,70 @@ impl std::fmt::Display for MagnetLink {
     }
 }
 
+impl PartialEq for MagnetLink {
+    fn eq(&self, other: &Self) -> bool {
+        self.query == other.query
+    }
+}
+
+#[cfg(feature = "sea_orm")]
+impl From<MagnetLink> for sea_orm::sea_query::Value {
+    fn from(m: MagnetLink) -> Self {
+        Self::String(Some(m.to_string()))
+    }
+}
+
+#[cfg(feature = "sea_orm")]
+impl sea_orm::TryGetable for MagnetLink {
+    fn try_get_by<I: sea_orm::ColIdx>(
+        res: &sea_orm::QueryResult,
+        index: I,
+    ) -> Result<Self, sea_orm::error::TryGetError> {
+        let val: String = res.try_get_by(index)?;
+        MagnetLink::new(&val).map_err(|e| {
+            sea_orm::error::TryGetError::DbErr(sea_orm::DbErr::TryIntoErr {
+                from: "String",
+                into: "MagnetLink",
+                source: std::sync::Arc::new(e),
+            })
+        })
+    }
+}
+
+#[cfg(feature = "sea_orm")]
+impl sea_orm::sea_query::ValueType for MagnetLink {
+    fn try_from(v: sea_orm::Value) -> Result<Self, sea_orm::sea_query::ValueTypeErr> {
+        match v {
+            // TODO: What to do with None String?
+            // This should probably work with Option<MagnetLink> but not with MagnetLink
+            // but i have no idea how sea orm works...
+            sea_orm::Value::String(Some(s)) => {
+                MagnetLink::new(&s).map_err(|_e| sea_orm::sea_query::ValueTypeErr)
+            }
+            _ => Err(sea_orm::sea_query::ValueTypeErr),
+        }
+    }
+
+    fn type_name() -> String {
+        "MagnetLink".to_string()
+    }
+
+    fn array_type() -> sea_orm::sea_query::ArrayType {
+        sea_orm::sea_query::ArrayType::String
+    }
+
+    fn column_type() -> sea_orm::sea_query::ColumnType {
+        sea_orm::sea_query::ColumnType::String(sea_orm::sea_query::table::StringLen::None)
+    }
+}
+
+#[cfg(feature = "sea_orm")]
+impl sea_orm::sea_query::Nullable for MagnetLink {
+    fn null() -> sea_orm::sea_query::Value {
+        sea_orm::sea_query::Value::String(None)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
