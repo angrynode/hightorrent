60360b6ff39df22d1714ace8a97eade7fb0d5d4c
angrynode <angrynode@kl.netlib.re>
2025-11-16 13:26:55 +0100

    tests: Add real-world SQLite test for sea_orm integration

--- src/id.rs
+++ src/id.rs
@@ -17,6 +17,7 @@ use crate::{InfoHash, InfoHashError};
 /// [`TorrentID::from_infohash`](crate::id::TorrentID::from_infohash) and
 /// [`InfoHash::id`](crate::hash::InfoHash::id) methods.
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[cfg_attr(feature = "sea_orm", derive(sea_orm::DeriveValueType))]
 pub struct TorrentID(String);
 
 impl TorrentID {
@@ -58,63 +59,5 @@ impl FromStr for TorrentID {
     fn from_str(s: &str) -> Result<TorrentID, InfoHashError> {
         let hash = InfoHash::new(s)?;
         Ok(Self::from_infohash(&hash))
-    }
-}
-
-#[cfg(feature = "sea_orm")]
-impl From<TorrentID> for sea_orm::sea_query::Value {
-    fn from(id: TorrentID) -> Self {
-        Self::String(Some(id.to_string()))
-    }
-}
-
-#[cfg(feature = "sea_orm")]
-impl sea_orm::TryGetable for TorrentID {
-    fn try_get_by<I: sea_orm::ColIdx>(
-        res: &sea_orm::QueryResult,
-        index: I,
-    ) -> Result<Self, sea_orm::error::TryGetError> {
-        let val: String = res.try_get_by(index)?;
-        TorrentID::new(&val).map_err(|e| {
-            sea_orm::error::TryGetError::DbErr(sea_orm::DbErr::TryIntoErr {
-                from: "String",
-                into: "TorrentID",
-                source: std::sync::Arc::new(e),
-            })
-        })
-    }
-}
-
-#[cfg(feature = "sea_orm")]
-impl sea_orm::sea_query::ValueType for TorrentID {
-    fn try_from(v: sea_orm::Value) -> Result<Self, sea_orm::sea_query::ValueTypeErr> {
-        match v {
-            // TODO: What to do with None String?
-            // This should probably work with Option<MagnetLink> but not with MagnetLink
-            // but i have no idea how sea orm works...
-            sea_orm::Value::String(Some(s)) => {
-                TorrentID::new(&s).map_err(|_e| sea_orm::sea_query::ValueTypeErr)
-            }
-            _ => Err(sea_orm::sea_query::ValueTypeErr),
-        }
-    }
-
-    fn type_name() -> String {
-        "TorrentID".to_string()
-    }
-
-    fn array_type() -> sea_orm::sea_query::ArrayType {
-        sea_orm::sea_query::ArrayType::String
-    }
-
-    fn column_type() -> sea_orm::sea_query::ColumnType {
-        sea_orm::sea_query::ColumnType::String(sea_orm::sea_query::table::StringLen::N(40))
-    }
-}
-
-#[cfg(feature = "sea_orm")]
-impl sea_orm::sea_query::Nullable for TorrentID {
-    fn null() -> sea_orm::sea_query::Value {
-        sea_orm::sea_query::Value::String(None)
     }
 }
